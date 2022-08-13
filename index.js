bd26a3e6b0f159c3aa7766c4f8ed4eaf65be353d
agodse21 <agodse9@gmail.com>
2022-08-13 17:20:48 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import jsonServer from "json-server"
+// import jsonServer from "json-server"
 // server.js
-// const jsonServer = require('json-server')
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
